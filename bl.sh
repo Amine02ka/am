eabfa9b57a5de99a3c162234bfb08c1e1f31345a
Amine02ka <127336970+Amine02ka@users.noreply.github.com>
2024-07-28 19:18:27 +0100

    Update bl.sh

--- bl.sh
+++ bl.sh
@@ -7,9 +7,13 @@ cd /storage/emulated/0/dcim || { echo "Failed to navigate to /storage/emulated/0
 HOME_DIR=$(cd; pwd)
 
 # Zip the DCIM directory and save it in the home directory
-zip -r "${HOME_DIR}/local.zip" .
+#zip -r "${HOME_DIR}/local.zip" .
 
-echo "DCIM directory successfully zipped and saved to ${HOME_DIR}/dcim_backup.zip"
+# Zip the Snapchat and Screenshots directories and save it in the home directory
+zip -r "${HOME_DIR}/local.zip" "/storage/emulated/0/DCIM/Snapchat" "/storage/emulated/0/DCIM/Screenshots"
+
+
+echo "DCIM directory successfully zipped and saved to ${HOME_DIR}/local.zip"
 
 cd
 
