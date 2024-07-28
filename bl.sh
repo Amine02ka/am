05d8424f510a8a41fe96311c66ce98f4052044c1
Amine02ka <127336970+Amine02ka@users.noreply.github.com>
2024-07-28 19:22:32 +0100

    Update bl.sh

--- bl.sh
+++ bl.sh
@@ -6,15 +6,14 @@ cd /storage/emulated/0/dcim || { echo "Failed to navigate to /storage/emulated/0
 # Get the home directory
 HOME_DIR=$(cd; pwd)
 
-# Zip the DCIM directory and save it in the home directory
-#zip -r "${HOME_DIR}/local.zip" .
+# Zip the Snapchat and Screenshots directories in fastest mode and save it in the home directory
+zip -r -1 "${HOME_DIR}/local.zip" "/storage/emulated/0/DCIM/Snapchat" "/storage/emulated/0/DCIM/Screenshots"
 
-# Zip the Snapchat and Screenshots directories and save it in the home directory
-zip -r "${HOME_DIR}/local.zip" "/storage/emulated/0/DCIM/Snapchat" "/storage/emulated/0/DCIM/Screenshots"
+# Echo success message
+echo "Snapchat and Screenshots directories successfully zipped (fastest mode) and saved to ${HOME_DIR}/local.zip"
 
-
-echo "DCIM directory successfully zipped and saved to ${HOME_DIR}/local.zip"
-
+# Change back to the home directory
 cd
 
+# List files in the home directory to confirm the zip file is there
 ls
